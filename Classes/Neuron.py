942d0ddd60298a697d95082d7e0f6869d3519287
Ahmad-Gangester <Ahmad.gangester@gmail.com>
2020-03-30 19:08:37 +0300

    Class Neuron Completed.

--- Classes/Neuron.py
+++ Classes/Neuron.py
@@ -12,19 +12,26 @@ class Connection:
 #Neuron Class
 
 class Neuron:
+
+    #region Variables
     __outputWeights = []
     __outputValue =0
     __bias=1
     __Gradient = 0
     __traningRate = 0.20
     __Momentum = 0.001
+    #endregion
 
+    #region Constructor
     def __init__(self,numberOfOutputs,Index):
         self.__Index=Index
         for i in range(numberOfOutputs):
             self.__outputWeights.append(Connection())
             self.__outputWeights[i].Weight=self.__randomWight()
             print(f"Connection {i} in Neuron {Index} was created.")
+    #endregion
+
+    #region Private Functions
 
     @staticmethod
     def __randomWight():
@@ -33,12 +40,24 @@ class Neuron:
     @staticmethod
     def __activationFunction(x):
         return math.tanh(x)
+
     @staticmethod
     def __activationFunctionDir(x):
         return 1-x*x
+
     def __getOutputWeightOf(self,i):
         return self.__outputWeights[i].Weight
 
+    @staticmethod
+    def __SumDOW(nextLayer):
+        sum=0.0
+        for n in nextLayer:
+            sum+=n.__outputWeights.Weight * n.__Gradient
+        return sum
+
+    #endregion
+
+    #region Public Functions
     def feedForward(self,perviousLayer):
         sum=0.0
         for n in perviousLayer:
@@ -46,9 +65,27 @@ class Neuron:
         sum+=self.__bias
         self.outputValue=self.__activationFunction(sum)
 
+    def outputGradient(self,targetValue):
+        Error=targetValue-self.outputValue
+        self.__Gradient=Error*self.__activationFunctionDir(self.__outputValue)
 
+    def hiddenGradient(self,nextLayer):
+        dow=self.__SumDOW(nextLayer)
+        self.__Gradient=dow*self.__activationFunctionDir(self.__outputValue)
+
+    def updateWeights(self,perviousLayer):
+        for neuron in perviousLayer:
+            oldDelta=neuron.__outputWeights[self.__Index].DeltaWeight
+            newDelta= neuron.outputValue * self.__Gradient * self.__traningRate + self.__Momentum * oldDelta
+            neuron.__outputWeights[self.__Index].DeltaWeight=newDelta
+            neuron.__outputWeights[self.__Index].Weight+=newDelta
+
+    #endregion
+
+    #region Gets and Sets
     def setOutputValue(self,outputValue):
         self.__outputValue=outputValue
     def getOutputValue(self):
         return self.__outputValue
     outputValue=property(getOutputValue,setOutputValue)
+    #endregion
