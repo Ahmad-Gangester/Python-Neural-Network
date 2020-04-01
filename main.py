d1bb34c3943bc62c18ac157c3da666446033d6dd
Ahmad-Gangester <Ahmad.gangester@gmail.com>
2020-04-01 20:23:37 +0300

    Updated with Data and Trainer

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-from Classes.Network import Network
-import random
+from Classes.Trainer import Trainer
+import os
+from pathlib import Path
 
 def main():
-    topology = [2,4,1]
-    inputs=[]
-    targets=[]
-    net = Network(topology)
-    for i in range(4000):
-        inputs.clear()
-        targets.clear()
-        print(f"Pass: {i}")
-        n1=random.randint(0,1)
-        n2=random.randint(0,1)
-        t=n1^n2
-        inputs.append(n1)
-        inputs.append(n2)
-        targets.append(t)
-        net.feedForward(inputs)
-        print(f"inputs: {n1} {n2}")
-        r=net.getResult()
-        print(f"results: {r}")
-        net.backPropagation(targets)
-        print(f"target: {t}")
-        print(f"Avg error : {net.AvgError}\n")
+    cur_path=Path(__file__).parent.absolute()
+    print(str(cur_path))
+    Ai=Trainer(str(cur_path) + "\\Data")
+    Ai.Train()
 
 if(__name__ == "__main__"):
     main()
